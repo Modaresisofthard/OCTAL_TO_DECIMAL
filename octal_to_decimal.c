635a21db3b5c90f670b8b6604e0396bc1a2d4cc7
Seyed Mohammad Amin Modaresi <108132583+Modaresisofthard@users.noreply.github.com>
2022-06-27 20:08:12 +0430

    Add files via upload

---
+++ octal_to_decimal.c
@@ -0,0 +1,12 @@
+/*OCTAL TO DECIMAL*/
+int main()
+{
+	int octal;
+
+	printf("OCTAL TO DECIMAL\n");
+	printf("Enter a number: ");
+	scanf("%o",&octal);
+	printf("decimal=%d\n",octal);
+	
+	return (0);
+}
